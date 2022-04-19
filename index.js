0fdcf425ec1569cf1a9cd823c339eee72be5a018
loadninja <97249365+loadninja@users.noreply.github.com>
2022-04-19 12:04:53 -0400

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,7 +0,0 @@
-const dateStr = '2022-07-21T09:35:31.820Z';
-
-const date = new Date(dateStr);
-console.log(date); // 👉️ Thu Jul 21 2022 12:35:31 GMT+0300
-
-const result = new Date(date.toISOString().slice(0, -1));
-console.log(result); // 👉️ Thu Jul 21 2022 09:35:31 GMT+0300
